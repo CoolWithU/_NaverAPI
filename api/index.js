4d3a1ffef49d213c45a8862ee6db50498d285130
TaeSeong <idhts2081@gmail.com>
2024-08-22 19:29:51 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('/shorten?url=[URL]');
+  res.send('https://github.com/NewJean-s/NaverAPI');
 });
 
 app.get('/shorten', (req, res) => {
