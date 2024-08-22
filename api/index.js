0b530fdfb2a30cf8b5f5fcdcdc227152469dfa81
TaeSeong <idhts2081@gmail.com>
2024-08-22 19:22:30 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,36 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  const path = `/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`<p>Hello! Go to item: <a href="${path}">${path}</a></p>`);
+  res.send('Made By deepl');
 });
 
-app.get('/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`<p>Item: ${slug}</p><a href="/">Go back</a>`);
+app.get('/shorten', (req, res) => {
+  var url = req.query.url;
+  
+  async function generate(link) {
+    const resp = await fetch(
+      `https://me2do.naver.com/common/requestJsonpV2.nhn?svcCode=0&url=https://link.naver.com/bridge?url=${link}`,
+      { method: 'POST', headers: { Referer: 'link.naver.com' } },
+    );
+    
+    const data = await resp.text();
+    const json = JSON.parse(data.trim().slice(1, -1));
+    const result = `{"result":{"data":"${json.result.httpsUrl}"}}`
+    res.json(JSON.parse(result))
+  };
+
+  generate(url)
+});
+
+app.listen(port, () => {
+  console.log(`Server listening`);
 });
 
 module.exports = app;
